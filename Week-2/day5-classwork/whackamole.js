728d6d87cbcfcc8de7fa36da4deef33f59bfcbf1
gabebierman <gabebierman@gmail.com>
2022-09-30 15:04:35 -0500

    choose a difficulty

--- Week-2/day5-classwork/whackamole.js
+++ Week-2/day5-classwork/whackamole.js
@@ -1,19 +1,3 @@
-// highlightDiv();
-
-//? Build a whack-a-mole game:
-
-// Have a grid of divs / containers on your page. They should all be the same size and be at least a 5x5 grid although you can add more if you would like
-// Have a start button that the user can click to start the game
-// When the game starts, show a timer for X amount of seconds. X can be any value you would like but should be no more than 30 seconds.
-// Every second or every random amount of time, have one of the divs light up by adding a class to it. After a reasonable (your call) amount of time remove the color change to that div and have it revert to normal.
-// If a user clicks one of the divs while it is lit up, a score counter at the top should increment by one.
-// After the timer ends, the cycle should stop and the user should see their score and have the ability to start a new game. Starting a new game should reset the score and start over.
-// If you would like / have time add the ability to choose a difficulty when the user starts the game.
-
-//* click when box is lit up
-// if click is true while backgroundcolor is x then hitCounter++
-// if e => e.target.backgroundColor === highlightColor {hitCounter++}
-
 const hitCount = { hit: 1 };
 
 let gameActive = false;
@@ -22,9 +6,9 @@ let timer = 10;
 
 let timeRef = 1000;
 
-let butStart = document.querySelectorAll("#start");
-
 let difTime;
+
+let butStart = document.querySelectorAll("#start");
 
 let divClick = document.querySelectorAll("div");
 
@@ -66,6 +50,10 @@ function playGame() {
                 highlightDiv();
             }, 666);
             difTime = 500;
+        } else {
+            document.getElementById("start").innerText =
+                "Please choose a difficulty";
+            timer = 0;
         }
 
         //game time
