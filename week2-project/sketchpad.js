982c2c0c37b9cb21f96ab0ccab5961948fc8c3eb
gabebierman <gabebierman@gmail.com>
2022-10-01 12:35:32 -0500

    styling canvas draw

--- week2-project/sketchpad.js
+++ week2-project/sketchpad.js
@@ -1,7 +1,3 @@
-//- Have a canvas HTML element
-
-//- Have 3 preset colors of your choice (to the left of the canvas)
-
 //- Allow the user to select any of the three colors and use some form of CSS (in this case the white border around red) to show which
 //color is selected
 
@@ -19,3 +15,44 @@
 //- Feel free to add more functionality, such as a way to clear the canvas, change the stroke size, add a better cursor etc.
 
 //! pick color from preset - colorSelect(e) => e.target.backgroundColor
+
+//! var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value;
+//slider.oninput = function() {output.innerHTML = this.value;}
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext();
+let coord = { x: 0, y: 0 };
+
+document.addEventListener("mousedown", start);
+document.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+
+function resize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+function reposition(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
+}
+function start(event) {
+    document.addEventListener("mousemove", draw);
+    reposition(event);
+}
+function stop() {
+    document.removeEventListener("mousemove", draw);
+}
+function draw(event) {
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#ACD3ED";
+    ctx.moveTo(coord.x, coord.y);
+    reposition(event);
+    ctx.lineTo(coord.x, coord.y);
+    ctx.stroke();
+}
