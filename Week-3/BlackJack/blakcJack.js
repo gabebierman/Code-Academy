3bd4e13b3cd91e8f6252d254acab3d05f5eb0afa
gabebierman <gabebierman@gmail.com>
2022-10-06 15:36:06 -0500

    build deck and shuffle

---
+++ Week-3/BlackJack/blakcJack.js
@@ -0,0 +1,67 @@
+// declare deck of cards
+const suit = ["diamonds", "hearts", "spades", "clubs"];
+const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+];
+
+let deck = new Array();
+
+function initDeck() {
+    deck = new Array();
+    for (let i = 0; i < suit.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            let weight = parseInt(values[j]);
+            if (values[j] === "J" || values[j] === "Q" || values[j] === "K") {
+                weight = 10;
+            } else if (values[j] === "A") {
+                weight = 11;
+            }
+            let card = { value: values[j], suit: suit[i], wieght: weight };
+            deck.push(card);
+        }
+    }
+    return deck;
+}
+// shuffle deck of cards
+
+function shuffleDeck() {
+    console.log(deck);
+    for (let i = 0; i < 1000; i++) {
+        let card1 = Math.floor(Math.random() * deck.length);
+        let card2 = Math.floor(Math.random() * deck.length);
+        let random = deck[card1];
+        deck[card1] = deck[card2];
+        deck[card2] = random;
+    }
+    return deck;
+}
+
+//computer player
+
+//user player
+
+//start the game
+
+//deal the hand
+
+//render cards and add to hand
+
+//hit button
+
+//stay button
+
+//determine outcome
+
+//add cards to discard
